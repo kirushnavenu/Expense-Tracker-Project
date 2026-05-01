5deda868930803b1ec604ce528807a4bf181850c
Kanthasamy Kirushnavenu <UT011648@unicomtic.com>
2026-05-01 16:48:01 +0530

    created index css and script

---
+++ script.js
@@ -0,0 +1,5 @@
+const DiscriptionInput=document.getElementById("Description");
+const AmountInput=document.getElementById("Amount");
+const AddTRanstationButton=document.getElementById("Add Transtation");
+const transtationList=document.getElementById("transtation-list");
+const balanceDisply=document.getElementById("balance");
